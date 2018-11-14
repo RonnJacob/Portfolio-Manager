d56ac837b3f4e631ea403bfcf7aa9106d7ac1d51
Ronn Jacob <jacob.r@husky.neu.edu>
2018-11-14 17:07:03 -0500

    Main class docs

--- src/howtoinvest/Main.java
+++ src/howtoinvest/Main.java
@@ -7,14 +7,26 @@ import howtoinvest.model.IPortfolioManager;
 import howtoinvest.model.StockPortfolio;
 import howtoinvest.model.StockPortfolioManager;
 
+/**
+ * This is the main class where the control is given to the HowToInvestController controller which
+ * would accept a IPortfolioManager based model and open the Portfolio manager for a user to perform
+ * operations related to investing in stocks of an organization and maintenance of various
+ * portfolios.
+ */
 public class Main {
 
   public static void main(String[] args) {
 
-      IPortfolioManager<StockPortfolio> portfolioManager;
-      portfolioManager = new StockPortfolioManager();
-      new HowToInvestController<>(new InputStreamReader(System.in), System.out)
-              .openPortfolioManager(portfolioManager);
+    /**
+     * Creation of PortfolioManager object which would be the model of the program.
+     */
+    IPortfolioManager<StockPortfolio> portfolioManager;
+    portfolioManager = new StockPortfolioManager();
+    /**
+     * Controller is given the control with the above model as the argument.
+     */
+    new HowToInvestController<>(new InputStreamReader(System.in), System.out)
+            .openPortfolioManager(portfolioManager);
 
 
   }
